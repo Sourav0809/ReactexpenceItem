eda0715ea142cfa456be6afb3fe7c1c815ac4cab
Sourav0809 <spathak7431@gmail.com>
2023-06-08 10:56:47 +0530

    First Commit

---
+++ src/Components/ExpenceItem.js
@@ -0,0 +1,13 @@
+const ExpenceItem = () => {
+    return (
+        <>
+            <h1>Food RS 100</h1>
+            <h1>Petrol Rs 100</h1>
+            <h1>Movie Rs 200</h1>
+
+        </>
+    )
+}
+
+
+export default ExpenceItem;
