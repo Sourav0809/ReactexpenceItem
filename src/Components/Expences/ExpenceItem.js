7cd161704dd9c567430a21ebc6d98738ad4a2c62
Sourav0809 <spathak7431@gmail.com>
2023-06-13 17:49:11 +0530

    Delete Btn Added

--- src/Components/Expences/ExpenceItem.js
+++ src/Components/Expences/ExpenceItem.js
@@ -4,6 +4,10 @@ import ExpenceDate from "./ExpenceDate";
 import React from "react";
 
 
+const onlcickHandeler = () => {
+    console.log("clicked")
+}
+
 const ExpenceItem = (props) => {
 
     return (
@@ -13,6 +17,7 @@ const ExpenceItem = (props) => {
                 <div className="expenceitems"><h2>{props.Name}</h2></div>
                 <div className="expenceitems"><h2>{props.Price}</h2></div>
                 <div className="expenceDate"><ExpenceDate Date={props.Date} /></div>
+                <div className="btn"><button onClick={onlcickHandeler}>Delete Btn</button></div>
             </div>
 
 
