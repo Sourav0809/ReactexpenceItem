29b82410bc039f1ef069ed339533048d3276dad9
Sourav0809 <spathak7431@gmail.com>
2023-06-15 01:10:44 +0530

    state added for handel different inputs

--- src/Components/NewExpence/ExpenceForm.js
+++ src/Components/NewExpence/ExpenceForm.js
@@ -1,25 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ExpenceForm.css'
 
-const expenceNameInput = (e) => {
-    console.log(e.target.value)
-}
-const expencePriceInput = (e) => {
-    console.log(e.target.value)
-}
-const expenceDateInput = (e) => {
-    console.log(e.target.value);
-}
+
 
 const ExpenceForm = () => {
+
+    const [enteredTitle, setEnteredTitle] = useState('')
+    const [enteredPrice, setEnteredPrice] = useState('')
+    const [enteredDate, setEnteredDate] = useState('')
+
+    const expenceNameHandeler = (e) => {
+        setEnteredTitle(e.target.value)
+    }
+
+    const expencePriceHandeler = (e) => {
+        setEnteredPrice(e.target.value)
+    }
+
+    const expenceDateHandeler = (e) => {
+        setEnteredDate(e.target.value)
+    }
+
+
     return <div className='ExpenceForm'>
         <form className='main-form'>
             <label className='form-label'>ExpenceName</label>
-            <input type='text' className='form-input ' onChange={expenceNameInput} />
+            <input type='text' className='form-input ' onChange={expenceNameHandeler} />
             <label className='form-label'>ExpencePrice</label>
-            <input type='number' className='form-input' onChange={expencePriceInput} />
+            <input type='number' className='form-input' onChange={expencePriceHandeler} />
             <label className='form-label'>ExpenceDate</label>
-            <input type='date' className='form-input ' onChange={expenceDateInput} />
+            <input type='date' className='form-input ' onChange={expenceDateHandeler} />
             <input type='submit' className='input-btn' />
         </form>
     </div>
