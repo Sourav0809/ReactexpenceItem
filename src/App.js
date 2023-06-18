6bfd857cfd91392a02807ac1ffd19e42b3bbb0b3
Sourav0809 <spathak7431@gmail.com>
2023-06-18 11:56:12 +0530

    Filtering Expences

--- src/App.js
+++ src/App.js
@@ -8,33 +8,37 @@ import ExpenceFiiter from "./Components/Expences/ExpenceFilter";
 
 const App = () => {
   const Expences = [
-    { id: 1, expenceName: "Food", Price: 450, date: new Date(2021, 5, 20) },
-    { id: 2, expenceName: "Wooden Table", Price: 600, date: new Date(2022, 5, 12) },
-    { id: 3, expenceName: "Toilet Papper", Price: 800, date: new Date(2021, 6, 12) },
-    { id: 4, expenceName: "Burger", Price: 100, date: new Date(2021, 10, 10) },
-    { id: 5, expenceName: "Petrol", Price: 4150, date: new Date(2021, 5, 12) },
-    { id: 6, expenceName: "Movies", Price: 250, date: new Date(2021, 4, 12) }
+    { id: 1, expenceName: "Food", Price: 450, date: new Date(2022, 5, 20) },
+    { id: 2, expenceName: "Wooden Table", Price: 600, date: new Date(2021, 5, 12) },
+    { id: 3, expenceName: "Toilet Papper", Price: 800, date: new Date(2020, 6, 12) },
+    { id: 4, expenceName: "Burger", Price: 100, date: new Date(2020, 10, 10) },
+    { id: 5, expenceName: "Petrol", Price: 4150, date: new Date(2019, 5, 12) },
+    { id: 6, expenceName: "Movies", Price: 250, date: new Date(2021, 4, 12) },
+    { id: 7, expenceName: "Shopping", Price: 1250, date: new Date(2018, 4, 12) },
+    { id: 8, expenceName: "Petrol", Price: 2500, date: new Date(2018, 4, 12) }
   ]
 
   const [currData, updatedData] = useState(Expences)
 
 
-  console.log(currData);
-
   const saveExpenceHandeler = (expence) => {
-    updatedData(
-      [...currData, expence]
+    updatedData((prevdata) => {
+      return [...prevdata, expence]
+
+    }
     )
   }
 
   // for filter 
 
-  const [filteredYear, setfilterdYear] = useState('')
+  const [filteredYear, setfilterdYear] = useState('2020')
   const filterChangeHandeler = (selectedYear) => {
     setfilterdYear(selectedYear)
-    console.log(selectedYear);
   }
 
+  const filteredExpence = Expences.filter((expenceValue) => {
+    return expenceValue.date.getFullYear().toString() === filteredYear
+  })
 
   return (
     <>
@@ -44,8 +48,8 @@ const App = () => {
       <div className="Container">
         <div className="items">
 
-          {currData.map((item) => {
-            return (<ExpenceItem Name={item.expenceName} Price={item.Price} Date={item.date} btnTitle={"Delete Expence"} />)
+          {filteredExpence.map((item) => {
+            return (<ExpenceItem key={item.id} Name={item.expenceName} Price={item.Price} Date={item.date} btnTitle={"Delete Expence"} />)
           })}
 
         </div>
